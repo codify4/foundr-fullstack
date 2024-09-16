c2c7e4c1cccafc97b384571960e346aea48fd52a
IjonK007 <kushta.joni@gmail.com>
2024-09-16 13:54:49 +0200

    feat: added basic page builder ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { SinglePageCreator } from "@/components/single-page-creator";
+
 export default function Home() {
   	return (
-		<div className="flex flex-col items-center justify-center h-svh text-3xl">Welcome to Foundr</div>
+		<div className="flex flex-col items-center justify-center h-svh">
+			<SinglePageCreator />
+		</div>
   	);
 }
