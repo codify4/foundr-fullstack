6b0b915477cd5cd47a47c971ed379ff0a5bc8ae1
codify4 <kushta.joni@gmail.com>
2024-09-25 17:25:12 +0200

    feat: new page schema

---
+++ db/schemas/page-schema.ts
@@ -0,0 +1,58 @@
+import { pgTable, serial, timestamp, varchar, text, integer, pgEnum } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { users } from './auth-schema';
+
+export const socialsEnum = pgEnum('socials', ['twitter', 'instagram', 'facebook', 'linkedin', 'github', 'website']);
+
+export const page = pgTable('page', {
+    id: serial('id').primaryKey(),
+    createdAt: timestamp('created_at').defaultNow(),
+    updatedAt: timestamp('updated_at').defaultNow(),
+    image: varchar('image'),
+    name: varchar('name'),
+    bio: text('bio'),
+    userId: integer('user_id').notNull(),
+});
+
+export const pageRelations = relations(page, ({ many, one }) => ({
+    socials: many(socialLink),
+    projects: many(project),
+    user: one(users, {
+        fields: [page.userId],
+        references: [users.id],
+    }),
+}));
+
+export const socialLink = pgTable('social_link', {
+    id: serial('id').primaryKey(),
+    createdAt: timestamp('created_at').defaultNow(),
+    updatedAt: timestamp('updated_at').defaultNow(),
+    type: socialsEnum('type'),
+    link: varchar('link'),
+    pageId: integer('page_id').notNull(),
+});
+
+export const socialLinkRelations = relations(socialLink, ({ one }) => ({
+    page: one(page, {
+        fields: [socialLink.pageId],
+        references: [page.id],
+    }),
+}));
+
+export const project = pgTable('project', {
+    id: serial('id').primaryKey(),
+    createdAt: timestamp('created_at').defaultNow(),
+    updatedAt: timestamp('updated_at').defaultNow(),
+    name: varchar('name'),
+    oneLiner: varchar('one_liner'),
+    url: varchar('url'),
+    mrr: varchar('mrr'),
+    pageId: integer('page_id').notNull(),
+});
+
+export const projectRelations = relations(project, ({ one }) => ({
+    page: one(page, {
+        fields: [project.pageId],
+        references: [page.id],
+    }),
+}));
