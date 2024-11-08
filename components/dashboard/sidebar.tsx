7ae87c711af4ad0ce209ab510fc0cc0701a91561
codify4 <kushta.joni@gmail.com>
2024-11-08 23:23:25 +0100

    added check out btn for mobile

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -104,54 +104,67 @@ const Sidebar = ({ slug }: { slug?: string }) => {
                     </div>
                 </nav>
             </div>
+            
+            <div className="flex lg:hidden flex-row items-center justify-between">
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button size="icon" className="flex lg:hidden bg-white dark:bg-neutral-900 text-black dark:text-white border hover:bg-gray-50">
+                            <Menu className="size-5" />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="left" className="bg-white dark:bg-neutral-900 text-black dark:text-white">
+                        <nav className="py-4 space-y-2">
+                            <div className="flex flex-col">
+                                <Link
+                                    className={`w-full text-left text-md px-4 py-3 rounded-md transition-colors cursor-pointer ${
+                                        pathname === '/dashboard'
+                                        ? 'bg-gray-50 dark:bg-neutral-800 text-black dark:text-white font-medium'
+                                        : 'text-neutral-500 hover:text-black dark:hover:bg-neutral-800'
+                                    }`}
+                                    href={`/dashboard`}
+                                >   
+                                    <Palette className="inline-block w-5 h-5 mr-2" />
+                                    Design
+                                </Link>
+                                <Link
+                                    className={`w-full text-left text-md px-4 py-3 rounded-md transition-colors ${
+                                        pathname === '/dashboard/analytics'
+                                        ? 'bg-gray-50 dark:bg-neutral-800 text-black dark:text-white font-medium'
+                                        : 'text-neutral-500 hover:text-black dark:hover:bg-neutral-800'
+                                    }`}
+                                    href={`/dashboard/analytics`}
+                                >   
+                                    <BarChart className="inline-block w-5 h-5 mr-2" />
+                                    Analytics
+                                </Link>
+                                <Link
+                                    className={`w-full text-left text-md px-4 py-3 rounded-md transition-colors ${
+                                        pathname === '/dashboard/settings'
+                                        ? 'bg-gray-50 dark:bg-neutral-800 text-black dark:text-white font-medium'
+                                        : 'text-neutral-500 hover:text-black dark:hover:bg-neutral-800'
+                                    }`}
+                                    href={`/dashboard/settings`}
+                                >
+                                    <Settings className="inline-block w-5 h-5 mr-2" />
+                                    Settings
+                                </Link>
+                            </div>
+                            <ThemeToggle />
+                        </nav>
+                    </SheetContent>
+                </Sheet>
 
-            <Sheet>
-                <SheetTrigger asChild>
-                    <Button size="icon" className="flex lg:hidden bg-white dark:bg-neutral-900 text-black dark:text-white border hover:bg-gray-50">
-                        <Menu className="size-5" />
+                <Link
+                    href={`/${slug}`}
+                    className="flex items-center justify-center "
+                    target="_blank"
+                >
+                    <Button className="bg-black hover:bg-primary">
+                        <SearchCheck className="size-5 mr-1"/>
+                        Check out
                     </Button>
-                </SheetTrigger>
-                <SheetContent side="left" className="bg-white dark:bg-neutral-900 text-black dark:text-white">
-                    <nav className="py-4 space-y-2">
-                        <div className="flex flex-col">
-                            <Link
-                                className={`w-full text-left text-md px-4 py-3 rounded-md transition-colors cursor-pointer ${
-                                    pathname === '/dashboard'
-                                    ? 'bg-gray-50 dark:bg-neutral-800 text-black dark:text-white font-medium'
-                                    : 'text-neutral-500 hover:text-black dark:hover:bg-neutral-800'
-                                }`}
-                                href={`/dashboard`}
-                            >   
-                                <Palette className="inline-block w-5 h-5 mr-2" />
-                                Design
-                            </Link>
-                            <Link
-                                className={`w-full text-left text-md px-4 py-3 rounded-md transition-colors ${
-                                    pathname === '/dashboard/analytics'
-                                    ? 'bg-gray-50 dark:bg-neutral-800 text-black dark:text-white font-medium'
-                                    : 'text-neutral-500 hover:text-black dark:hover:bg-neutral-800'
-                                }`}
-                                href={`/dashboard/analytics`}
-                            >   
-                                <BarChart className="inline-block w-5 h-5 mr-2" />
-                                Analytics
-                            </Link>
-                            <Link
-                                className={`w-full text-left text-md px-4 py-3 rounded-md transition-colors ${
-                                    pathname === '/dashboard/settings'
-                                    ? 'bg-gray-50 dark:bg-neutral-800 text-black dark:text-white font-medium'
-                                    : 'text-neutral-500 hover:text-black dark:hover:bg-neutral-800'
-                                }`}
-                                href={`/dashboard/settings`}
-                            >
-                                <Settings className="inline-block w-5 h-5 mr-2" />
-                                Settings
-                            </Link>
-                        </div>
-                        <ThemeToggle />
-                    </nav>
-                </SheetContent>
-            </Sheet>
+                </Link>
+            </div>
         </>
         
     )
